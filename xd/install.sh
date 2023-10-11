3d6aab4b8c0b9572f94aa8340c671e2cd0edd75f
Minn <qaqmango@outlook.com>
2023-10-11 18:05:16 +0800

    update: xd

---
+++ xd/install.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+###
+# project https://github.com/MetaCubeX/metacubexd
+###
+
+set -e
+# Initialize variables
+bindir=/home/xd
+
+
+wget_install() {
+  wget https://github.com/MetaCubeX/metacubexd/releases/latest/download/compressed-dist.tgz -O /tmp/compressed-dist.tgz
+  tar zxvf /tmp/compressed-dist.tgz -C $bindir
+}
+
+
+main() {
+  mkdir -p $bindir
+  rm -rf $bindir/*
+  wget_install
+
+  exit 0
+}
+
+uninstall() {
+  rm -rf $bindir
+  exit 0
+}
+
+help() {
+  echo -e "usage: install.sh ACTION [OPTION]...
+
+ACTION:
+install                   Install/Update sing-box
+remove                    Remove sing-box
+help                      Show help
+If no action is specified, then help will be selected
+
+"
+  exit 0
+}
+# Parse command line arguments
+for arg in "$@"; do
+  case $arg in
+    help)
+      action="help"
+      ;;
+    remove)
+      action="remove"
+      ;;
+    install)
+      action="install"
+      ;;
+    *)
+      echo "Invalid argument: $arg"
+      exit 1
+      ;;
+  esac
+done
+
+# Perform action based on user input
+case "$action" in
+  help)
+    help
+    ;;
+  remove)
+    uninstall
+    ;;
+  install)
+    main
+    ;;
+  *)
+    echo "No action specified. Exiting..."
+    ;;
+esac
+
+help
